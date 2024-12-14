53ac3c3be603f8e0ccf1a55ea291da6b3bd98417
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-12-14 22:11:11 +1100

    项目创建

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+export default {
+  // 箭头函数只有一个参数的时候，忽略括号
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  // 换行的方式
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  endOfLine: 'lf',
+  parser: 'typescript',
+}
