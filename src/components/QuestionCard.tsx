fbafaa9ae37bd3798c997b0cc5a26887c13bb0e8
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-12-17 20:59:04 +1100

    chore: bulid List

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+import styles from './QuestionCard.module.scss'
+type PropsType = {
+  _id: string
+  title: string
+  isPublished: boolean
+  isStar: boolean
+  answerCount: number
+  createAt: string
+}
+
+const QuestionCard: FC<PropsType> = (props: PropsType) => {
+  const { _id, title, isPublished, isStar, answerCount, createAt } = props
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <div className={styles.left}>
+          <a href='#'>{title}</a>
+        
+          </div>
+        <div className={styles.right}>
+         {isPublished ? <span className={styles.isPublished}>已发布</span>
+         : <span className={styles.isNotPublished}>未发布</span>}
+          <span className={styles.answerCount}>答卷 : {answerCount}</span>
+          <span className={styles.createAt}>创建于:{createAt}</span>
+  
+        </div>
+      </div>
+      <div className={styles['button-container']}>
+        <div className={styles.left}>
+          <button>编辑问卷</button>
+          <button>数据统计</button>
+        </div>
+        <div className={styles.right}>
+          <button>标星</button>
+          <button>复制</button> 
+          <button>删除</button>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default QuestionCard
---
+++ src/pages/List.tsx
@@ -0,0 +1,58 @@
+import { FC, useState } from 'react'
+import styles from './List.module.scss'
+import QuestionCard from '../components/QuestionCard'
+const rawQuestionList = [
+  {
+    _id: '1',
+    title: '问卷1',
+    isPublished: true,
+    isStar: true,
+    answerCount: 10,
+    createAt: '3月10 13:12',
+  },
+  {
+    _id: '2',
+    title: '问卷2',
+    isPublished: false,
+    isStar: true,
+    answerCount: 13,
+    createAt: '6月10 10:12',
+  },
+  {
+    _id: '3',
+    title: '问卷3',
+    isPublished: true,
+    isStar: false,
+    answerCount: 40,
+    createAt: '4月10 13:12',
+  },
+]
+const List: FC = () => {
+  const [questionlist, setquestionList] = useState(rawQuestionList)
+
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+         <h3>我的问卷</h3> 
+          </div>
+        <div className={styles.right}>
+          搜索
+        </div>
+      </div>
+      <div className={styles.content}>
+        {
+          questionlist.map((item) => {  
+            const { _id} = item
+            return (
+             <QuestionCard key={_id} {...item}/>
+            )
+          })
+        }
+      </div>
+      <div className={styles.footer}>分页 </div>
+    </>
+  )
+}
+
+export default List
